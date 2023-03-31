7a2afedcd91d6055232a907232ef009e632deaf2
Cujba Tiberiu Stefan <123594218+CujbaTiberiu@users.noreply.github.com>
2023-03-31 16:23:29 +0200

    final product 2.0

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import MyCarousel from './components/MyCarousel';
 import MyNav from './components/MyNav';
 import MyFooter from './components/MyFooter';
 import UnderNav from './components/UnderNav';
-import MyForm from './components/MyForm';
 
 function App() {
   const starTrek = "star%20trek"
